fc30a414a47b32721ee3c8ca66cdbed155727566
leanndemetro <leanndemetro@gmail.com>
2020-10-21 16:00:01 -0400

    removed the console log so api data doesn't return in console every time we node server.js, added a temporary search form and submit button and wrote code within members.js to log the search input value to the console for later use returning data from plants db

--- public/js/members.js
+++ public/js/members.js
@@ -5,3 +5,21 @@ $(document).ready(() => {
     $(".member-name").text(data.email);
   });
 });
+
+$(document).ready(() => {
+  // Getting references to our form and input
+  const searchForm = $("form.search");
+  const searchInput = $("input#search-input");
+
+  // When the signup button is clicked, we validate the email and password are not blank
+  searchForm.on("submit", event => {
+      event.preventDefault();
+      const searchData = {
+          search: searchInput.val().trim(),
+      };
+      console.log(searchData);
+
+      //send searchData value and call a function that searches through database and returns matching values
+  })
+
+});
